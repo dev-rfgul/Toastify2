e163af6c90661fd427414cb28bc851ceda2c903b
Muhammad Fahad <rfgul587@gmail.com>
2024-01-03 23:57:26 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const App = () => {
+  const notify = () => toast("All credits to Dev rf-gul");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={notify}>Login</button>
+      <ToastContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
